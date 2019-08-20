456fa497de1a25a5725ce140a3cea30d7d9c9fb9
github username <github email address>
2019-08-20 09:49:57 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,31 +10,50 @@ class Dog
 
     def self.create_table
         sql = << -SQL
-            CREATE TABLE dogs IF EXISTS NOT
+            CREATE TABLE IF NOT EXISTS dogs(
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
         SQL
         DB[:conn].execute(sql)
     end 
 
     def self.drop_table
-        sql = << -SQL
-            DROP TABLE dogs
-        SQL
+        sql = "DROP TABLE IF EXISTS dogs"
+        DB[:conn].execute(sql)
     end 
 
-    def new_from_db
-
+    def new_from_db(row)
+        id = row[0]
+        name = row[1]
+        breed = row[2]
+        self.new(id: id, name: name, breed: breed)
     end
 
-    def find_by_name
-
+    def find_by_name(name)
+        sql = << -SQL
+            SELECT *
+            FROM dogs
+            WHERE name = ?
+            LIMIT 1
     end 
 
     def update
-
+        sql = "UPDATE dogs SET name = ?, breed = ? WHERE id =?"
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
     end
 
     def save
-
+        if self.id
+            self.update
+        else 
+            sql = << -SQL
+                INSERT INTO dogs (name, breed)
+                VALUES (?, ?)
+            SQL
+            DB[:conn].execute(sql, self.name, self.breed)
+            @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end 
 
 end
